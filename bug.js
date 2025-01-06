f7c4b45939793719b5354382cdf4c33936bab201
BugHunter-X <askzenai@gmail.com>
2025-01-06 14:56:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,4 @@
+This error occurs when using the Expo CLI to build an Android APK. The build process fails with the error message: `FAILURE: Build failed with an exception. * What went wrong: Could not resolve all files for configuration ':app:debugImplementation'. > Could not find artifact com.google.firebase:firebase-bom:29.3.0.  Searched in the following locations:
+ - https://repo.maven.apache.org/maven2/com/google/firebase/firebase-bom/29.3.0/firebase-bom-29.3.0.pom
+ - https://jitpack.io/com/google/firebase/firebase-bom/29.3.0/firebase-bom-29.3.0.pom
+ - ...
